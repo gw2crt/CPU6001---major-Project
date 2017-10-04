e6df6a2da90bab734bce65dd6aeb9d800cb1ea39
Gareth <gw2crt@gmail.com>
2017-10-04 20:05:27 +0100

    Update to Repo - Testing Complete

--- raspberry_pi/python/scan.py
+++ raspberry_pi/python/scan.py
@@ -4,10 +4,26 @@ import time
 connection = obd.Async()
 
 # a callback that prints every new value to the console
+def new_spd(s):
+	print s.value
+
 def new_rpm(r):
     print r.value
 
+def new_tps(t):
+	print t.value
+
+def new_maf(m):
+	print m.value
+
+def new_ful(f):
+	print f.value
+
+connection.watch(obd.commands.SPEED, callback=new_spd)
 connection.watch(obd.commands.RPM, callback=new_rpm)
+connection.watch(obd.commands.THROTTLE_POS, callback=new_tps)
+connection.watch(obd.commands.MAF, callback=new_maf)
+connection.watch(obd.commands.ENGINE_LOAD, callback=new_ful)
 connection.start()
 
 # the callback will now be fired upon receipt of new values
