5418aef46ec19230f25dbb4a0c5048761ca85a2c
Gareth <gw2crt@gmail.com>
2017-10-04 19:16:49 +0100

    Added Speed, RPM, TPS, MAF, Engine Load queries to OBD327

--- raspberry_pi/python/sync.py
+++ raspberry_pi/python/sync.py
@@ -13,13 +13,27 @@ def new_tps(t):
 def new_speed(s):
     print s.value
 
+def new_maf(m):
+    print m.value
+
+def new_erun(e):
+    print r.value
+
+def new_fuel(f):
+    print f.value
+
+
+
 connection.watch(obd.commands.RPM, callback=new_rpm)
 connection.watch(obd.commands.THROTTLE_POS, callback=new_tps)
 connection.watch(obd.commands.SPEED, callback=new_speed)
+connection.watch(odb.commands.MAF, callback=new_maf)
+connection.watch(obd.commands.RUN_TIME, callback=new_erun)
+connection.watch(obd.commands.FUEL_RATE, connection=new_fuel)
 
 connection.start()
 
 # the callback will now be fired upon receipt of new values
 
-time.sleep(60)
-connection.stop()
+#time.sleep(0)
+#connection.stop()
