d2fad364fa64545b64f8c70ab327053497d4fe0e
Gareth <gw2crt@gmail.com>
2017-10-07 11:16:00 +0100

    Added GPIO Lib to set GPIO port 1 to on once vehicle is moving.

--- raspberry_pi/python/scan.py
+++ raspberry_pi/python/scan.py
@@ -1,5 +1,10 @@
 import obd
 import time
+import RPI.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW) #Setup to initial value as low as the vehicle will be stationary at setup.
 
 connection = obd.Async()
 
@@ -16,14 +21,14 @@ def new_tps(t):
 def new_maf(m):
 	print m.value
 
-def new_lod(l):
+def new_elod(l):
 	print f.value
 
 connection.watch(obd.commands.SPEED, callback=new_spd)
 connection.watch(obd.commands.RPM, callback=new_rpm)
 connection.watch(obd.commands.THROTTLE_POS, callback=new_tps)
 connection.watch(obd.commands.MAF, callback=new_maf)
-connection.watch(obd.commands.ENGINE_LOAD, callback=new_lod)
+connection.watch(obd.commands.ENGINE_LOAD, callback=new_elod)
 connection.start()
 
 # the callback will now be fired upon receipt of new values
