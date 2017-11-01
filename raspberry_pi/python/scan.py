300b18e40f40e3b30cc2feb79177408581e09abd
Gareth <gw2crt@gmail.com>
2017-11-01 16:08:41 +0000

    Mongo Dababase entry
    Added a method to  fill Mongo database with OBD2 data.

--- raspberry_pi/python/scan.py
+++ raspberry_pi/python/scan.py
@@ -1,10 +1,16 @@
-import RPI.GPIO as GPIO
+#import RPI.GPIO as GPIO
 import obd
 import time
+from pymongo import MongoClient
+import pprint
+import datetime
 
-GPIO.setmode(GPIO.BOARD)
+client = MongoClient('mongodb://gareth:1utmHy2HBYCKZWLO@testing-shard-00-00-ic2uk.mongodb.net:27017,testing-shard-00-01-ic2uk.mongodb.net:27017,testing-shard-00-02-ic2uk.mongodb.net:27017/test?ssl=true&replicaSet=testing-shard-0&authSource=admin')
+db=client.testdb
 
-GPIO.setup(40, GPIO.OUT, initial=GPIO.HIGH) #Setup to initial value as HIGH as the vehicle will be stationary at setup.
+#GPIO.setmode(GPIO.BOARD)
+
+#GPIO.setup(40, GPIO.OUT, initial=GPIO.HIGH) #Setup to initial value as HIGH as the vehicle will be stationary at setup.
 
 connection = obd.Async()
 
@@ -22,7 +28,7 @@ def new_maf(m):
 	print m.value
 
 def new_elod(l):
-	print f.value
+	print l.value
 
 connection.watch(obd.commands.SPEED, callback=new_spd)
 connection.watch(obd.commands.RPM, callback=new_rpm)
@@ -31,7 +37,27 @@ connection.watch(obd.commands.MAF, callback=new_maf)
 connection.watch(obd.commands.ENGINE_LOAD, callback=new_elod)
 connection.start()
 
-# Note: if s.value != 0: GPIO.output(1, LOW) <- If the vehicle isnt moving the GPIO is set to HIGH. this needs testing
+was_running = connection.running
+connection.stop()
+
+result = db.smartcarasync.insert_one(
+        {
+            "Vehicle": {
+                "Model": "Smart",
+                "Year": "2005",
+                "Type": "Roadster",
+                "RPM": "sausage"
+            }
+        }
+    )
+
+
+if was_running:
+    connection.start()
+
+
+
+# Note: if s.value.magnitude != 0: GPIO.output(1, LOW) <- If the vehicle isnt moving the GPIO is set to HIGH. this needs testing
 
 # the callback will now be fired upon receipt of new values
 
